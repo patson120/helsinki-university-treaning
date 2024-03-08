f90fa4dbec9e9e2792eecd6dbb5bb65d0a763a1c
patson120 <patrickkennenl@gmail.com>
2024-03-08 09:36:50 +0100

    Add news components

--- part8/book/src/main.tsx
+++ part8/book/src/main.tsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { gql, InMemoryCache, ApolloClient, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+// const query = gql`
+//   query{
+//     allBooks {
+//       title
+//       author
+//       published
+//     }
+//   }
+// `
+
+// client.query({ query })
+// .then((response) => {
+//   console.log(response.data)
+// })
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
